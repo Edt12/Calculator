ad38bc00cd172c5fffe330114b4e8b117545d5a3
Edt12 <104518243+Edt12@users.noreply.github.com>
2024-06-26 17:34:40 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Welcome to the calculator of doom" <<std::endl;
+    std::cout << "What mode would you like \n press 1 for divide\n press 2 for multiply \n press 3 for add" <<std::endl;
+    std::cin >>"GIVE ME THE NUMBER"
+    return 0;
+}
+
